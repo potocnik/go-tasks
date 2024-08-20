65d3854ecdb3bd341626bbf48956eb15a4b2a84a
Ales Potocnik <ales.potocnik@bjss.com>
2024-08-20 14:29:36 +0100

    More integration tests

--- tests/integration/workflow_none_test.go
+++ tests/integration/workflow_none_test.go
@@ -2,7 +2,6 @@ package integration_test
 
 import (
 	"bytes"
-	"fmt"
 	tasks "tasks/pkg/task_list"
 	"tasks/pkg/test_utils"
 	"testing"
@@ -55,6 +54,16 @@ func TestWorkflow_Push(t *testing.T) {
 		actual := tasks.SaveState(task_list)
 		test_utils.AssertEqual(t, actual.String(), expected)
 	})
+	t.Run("push multiple items", func(t *testing.T) {
+		expected := "[\"Task 1\",\"Task 2\",\"Task 3\",\"Task 4\",\"Task 5\",\"Task 6\",\"Task 7\",\"Task 8\",\"Task 9\",\"Task 10\"]"
+		data := bytes.NewBuffer([]byte("[\"Task 1\",\"Task 2\",\"Task 3\",\"Task 4\",\"Task 5\",\"Task 6\",\"Task 7\",\"Task 8\"]"))
+		var task_list = tasks.LoadState(data)
+		task_list = tasks.Push(task_list, "Task 9")
+		task_list = tasks.Push(task_list, "Task 10")
+		task_list = tasks.Push(task_list, "Task 11")
+		actual := tasks.SaveState(task_list)
+		test_utils.AssertEqual(t, actual.String(), expected)
+	})
 }
 
 func TestWorkflow_Pop(t *testing.T) {
@@ -70,11 +79,18 @@ func TestWorkflow_Pop(t *testing.T) {
 		expected := "[]"
 		data := bytes.NewBuffer([]byte("[\"One task\"]"))
 		var task_list = tasks.LoadState(data)
-		fmt.Println(task_list)
 		task_list, _ = tasks.Pop(task_list)
-		fmt.Println(task_list)
 		actual := tasks.SaveState(task_list)
-		fmt.Println(actual)
+		test_utils.AssertEqual(t, actual.String(), expected)
+	})
+	t.Run("pop multiple items", func(t *testing.T) {
+		expected := "[]"
+		data := bytes.NewBuffer([]byte("[\"One task\",\"Two tasks\"]"))
+		var task_list = tasks.LoadState(data)
+		task_list, _ = tasks.Pop(task_list)
+		task_list, _ = tasks.Pop(task_list)
+		task_list, _ = tasks.Pop(task_list)
+		actual := tasks.SaveState(task_list)
 		test_utils.AssertEqual(t, actual.String(), expected)
 	})
 }
